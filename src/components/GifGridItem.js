8afa7faa4ad033ff8a6355d2e201fa6a19d8dbaf
Carlos Andres Norato Gomez <carnoratgmail.com>
2021-07-26 10:06:12 +0200

    Despliege de GifExpert

---
+++ src/components/GifGridItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export  const GifGridItem = ({title, url, id}) => {
+
+
+    return (
+        <div className="card animate__animated animate__fadeIn">
+            <img src={url} alt={title}/>
+            <p>{title.toUpperCase()}</p>
+        </div>
+    )
+}
